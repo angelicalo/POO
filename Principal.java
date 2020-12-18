b8b9bddaaf9963b76543993519a38b2adcfa617a
Angelica Lourenco Oliveira <48502010+angelicalo@users.noreply.github.com>
2020-12-17 21:15:42 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,34 @@
+public class Principal
+{
+    public static void main(String args[])
+    {    
+    /*
+    Disciplina d = new Disciplina();
+    d.lerDados();
+    d.mostrarDados();    
+    */  
+    /* 
+    Funcionario f = new Funcionario();
+    f.lerDados();
+    f.mostrarDados();  
+    */
+    /* 
+    Aluno a = new Aluno();
+    a.lerDados();
+    a.mostrarDados(); 
+    */  
+    /*    
+    Curso c = new Curso();
+    c.lerDados();
+    c.mostrarDados();   
+    */   
+    Professor p = new Professor();
+    p.lerDados();
+    p.mostrarDados(); 
+    /*  
+    Tecnico t = new Tecnico();
+    t.lerDados();
+    t.mostrarDados();
+    */ 
+    }
+}
